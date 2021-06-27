10296f5abf6da1805a9e201535e860b8d09b2766
Luthianni Alves <57681298+Luthianni@users.noreply.github.com>
2021-06-27 15:15:25 -0300

    criado array dos filmes e o melhorando a interf.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +11,29 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome:'Luca (2021)',
+      lancamento:'17/06/2021',
+      duracao:'1h 41m',
+      classificacao:83,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1lk7QISmTam2iOzzUAQYs6voeFg.jpg',
+      generos:['Animação', 'Comédia', 'Família', 'Fantasia']
+    },
+    {
+      nome:'Velozes & Furiosos 9 (2021)',
+      lancamento:'24/06/2021',
+      duracao:'2h 25m',
+      classificacao:80,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8L4Mep3KDUK4ztUgf2HlPvUVzZy.jpg',
+      generos:['Ação', 'Aventura', 'Crime']
+    }
+  ];
+
+  constructor(public alertController: AlertController,
+     public toastController: ToastController) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -24,13 +49,22 @@ export class Tab1Page {
         }, {
           text: 'Sim, Favoritar!',
           handler: () => {
-            console.log('Confirm Okay');
+            this.presentToast();
           }
         }
       ]
     });
 
     await alert.present();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Filmes adicionado aos favoritos.',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
   }
 
 }
